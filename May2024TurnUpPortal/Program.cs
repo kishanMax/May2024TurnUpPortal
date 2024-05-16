7bcf10cb0b2c268ca59ac441e80ab35ff6093303
kishanMax <kishannavaratne@gmail.com>
2024-05-16 22:31:48 +1200

    TurnUp Portal Login Script

---
+++ May2024TurnUpPortal/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+IWebElement userNameElement = driver.FindElement(By.Id("UserName"));
+userNameElement.SendKeys("hari");
+
+IWebElement passwordElement = driver.FindElement(By.Id("Password"));
+passwordElement.SendKeys("123123");
+
+IWebElement loginButtonElement = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButtonElement.Click();
+
+
